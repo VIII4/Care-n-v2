a4c29146fb76a2f3cb04f88522507f8d22558291
Lay84 <Iyan.laylor@gmail.com>
2020-09-16 16:59:52 -0700

    migrated mlab to atlas, reconnected heroku and seeded database

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -9,11 +9,11 @@ const User = require("../models/users");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/caren");
 
 // =============== //
-// heroku mongoose //
+// mlab atlas //
 // =============== //
 // mongoose.connect(
 //   process.env.MONGODB_URI ||
-//   "mongodb://carenUser:groupProject3@ds145208.mlab.com:45208/heroku_kgv68jn2"
+//     "mongodb+srv://admin:CarenTest1234@cluster0.lp01o.mongodb.net/caren?retryWrites=true&w=majority"
 // );
 
 const issueSeed = [
