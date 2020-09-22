05b8ceb834bae385100f63c4f111d8d889f26c7c
Lay84 <Iyan.laylor@gmail.com>
2020-09-22 11:46:35 -0700

    test alerts for UI button functionality

--- client/src/components/UICard/index.js
+++ client/src/components/UICard/index.js
@@ -8,12 +8,17 @@ import UserProfileButton from "../UIButtons/UserProfileButton";
 
 export default function UICard({ onManualRefreshClick, onReportIssueClick }) {
   const size = "15vw";
+
+  function buttonAlert() {
+    alert("button Pressed");
+  }
+
   return (
     <div id="UIcontainer" className="d-flex">
       {/* Single Button on Left */}
       <div className="UIoverlay align-self-end">
         <span id="profile-button">
-          <a>
+          <a onClick={buttonAlert}>
             <UserProfileButton size={size} />
           </a>
         </span>
@@ -23,15 +28,15 @@ export default function UICard({ onManualRefreshClick, onReportIssueClick }) {
       <div id="right-buttons-list" className="UIoverlay align-self-end">
         <ul className="button-list ">
           <li className="button-list-item d-flex justify-content-end">
-            <a>
+            <a onClick={buttonAlert}>
               <IssueListButton size={"10vh"} />
             </a>
-            <a onClick={onManualRefreshClick}>
+            <a onClick={buttonAlert}>
               <RefreshButton size={"8vw"} />
             </a>
           </li>
           <li className="button-list-item">
-            <a>
+            <a onClick={buttonAlert}>
               <NewIssueButton size={"40vw"} />
             </a>
           </li>
