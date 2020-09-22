98d2c80f508a92a64a0e39e73097c0408f79b891
Lay84 <Iyan.laylor@gmail.com>
2020-09-22 16:26:38 -0700

    added alerts to test screen queries

--- client/src/components/UICard/index.js
+++ client/src/components/UICard/index.js
@@ -11,10 +11,13 @@ let size;
 //Helper function for media querie, set responsive size of UI
 const checkSize = () => {
   if (window.matchMedia("(max-width: 380px)").matches) {
+    alert("Mobile");
     size = 0.4;
   } else if (window.matchMedia("(max-width: 1024px)").matches) {
+    alert("tablet");
     size = 0.8;
   } else if (window.matchMedia("(max-width: 1440px)").matches) {
+    alert("desktop");
     size = 0.65;
   } else {
     size = 0.75;
