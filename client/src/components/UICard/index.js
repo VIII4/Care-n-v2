5a4f5739a40059cdfce38dca2742ee5a9660d75a
Lay84 <Iyan.laylor@gmail.com>
2020-09-22 21:15:21 -0700

    added temp handlers to open list card from new ui button

--- client/src/components/UICard/index.js
+++ client/src/components/UICard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 import IssueListButton from "../UIButtons/ListIssueButton";
@@ -6,9 +6,9 @@ import NewIssueButton from "../UIButtons/NewIssueButton";
 import RefreshButton from "../UIButtons/RefreshButton";
 import UserProfileButton from "../UIButtons/UserProfileButton";
 
-let size;
-
+//#region Responsive Helpers
 //Helper function for media querie, set responsive size of UI
+let size;
 const checkSize = () => {
   if (window.matchMedia("(max-width: 380px)").matches) {
     //alert("Mobile");
@@ -21,20 +21,68 @@ const checkSize = () => {
     size = 0.8;
   } else if (window.matchMedia("(max-width: 1440px)").matches) {
     //alert("desktop");
-    size = 0.65;
+    size = 0.6;
   } else {
     size = 0.75;
   }
 };
-
 //Run check size at runtime
 checkSize();
 //event listener for screen resize
 window.addEventListener("resize", function () {
   checkSize();
 });
+//#endregion
+
+//#region UI Menu Helpers
+const CloseCardInnards = () => {
+  // collect html collection --> convert to array
+  const cardInnardsList = Array.prototype.slice.call(
+    document.getElementsByClassName("cardInnards")
+  );
+
+  // hide all card innards with common className
+  cardInnardsList.forEach((element) => {
+    element.style.display = "none";
+  });
+};
+
+const CloseCard = () => {
+  // hide card containter
+  document.getElementById("cardContainer").style.visibility = "hidden";
+  CloseCardInnards();
+};
+
+const OpenCard = (event, type) => {
+  // first close any open cards
+  CloseCardInnards();
+
+  // open card container
+  document.getElementById("cardContainer").style.visibility = "visible";
+
+  // insert voting card innards
+  document.getElementById("voteCardContent").style.display = "block";
+};
+
+CloseCardInnards();
+//#endregion
 
 export default function UICard({ onManualRefreshClick, onReportIssueClick }) {
+  //State for toggle
+  const [bool, setBool] = useState(false);
+
+  const OpenToggle = () => {
+    // toggle boolean value to either close or open card
+
+    if (!bool) {
+      OpenCard();
+      setBool(false);
+    } else {
+      CloseCard();
+      setBool(true);
+    }
+  };
+
   return (
     <div id="UIcontainer" className="d-flex">
       {/* Single Button on Left */}
@@ -50,7 +98,7 @@ export default function UICard({ onManualRefreshClick, onReportIssueClick }) {
       <div id="right-buttons-list" className="UIoverlay align-self-end">
         <ul className="button-list ">
           <li className="button-list-item d-flex justify-content-end">
-            <a className="ui-button">
+            <a className="ui-button" onClick={OpenToggle}>
               <IssueListButton size={size} />
             </a>
             <a
